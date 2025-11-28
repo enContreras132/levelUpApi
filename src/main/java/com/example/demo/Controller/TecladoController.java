17cefabe8c4aaba22047198d5f641120e1f7669b
Fabianxarii <fa.cuevast@duocuc.cl>
2025-11-27 23:28:02 -0300

    Teclado Service, Controller
    Teclado Service, Controller

--- src/main/java/com/example/demo/Controller/TecladoController.java
+++ src/main/java/com/example/demo/Controller/TecladoController.java
@@ -1,4 +1,45 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.TecladoModel;
+import com.example.demo.Service.TecladoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/teclado")
 public class TecladoController {
+
+    @Autowired
+    TecladoService TecladoService;
+
+    @GetMapping
+    public String getAllTeclado(){
+        return TecladoService.getAllTeclado();
+    }
+
+    @GetMapping("/{id}")
+    public String getTecladobyId(@PathVariable int id){
+        return TecladoService.getTecladoById(id);
+    }
+
+    @PostMapping
+    public  String addTeclado(@RequestBody TecladoModel tecladoModel){
+        return TecladoService.addTeclado(tecladoModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTeclado(@PathVariable int id){
+        return TecladoService.deleteTeclado(id); // Llama al servicio con el ID
+    }
+    @PutMapping("/{id}")
+    public String updateTeclado(@PathVariable int id, @RequestBody TecladoModel teclado){
+        return TecladoService.updateTeclado(id, teclado);
+    }
+
+
+
+
+
+
 }
---
+++ src/main/java/com/example/demo/Service/TecladoService.java
@@ -0,0 +1,98 @@
+package com.example.demo.Service;
+
+import com.example.demo.Model.TecladoModel;
+import com.example.demo.Repository.TecladoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TecladoService {
+
+    @Autowired
+    TecladoRepository TecladoRepository;
+
+    //Listar
+    public String getAllTeclado() {
+        String output = "";
+        for (TecladoModel tm : TecladoRepository.findAll()){
+            output += "id: "+tm.getId()+"\n";
+            output += "nombre: "+tm.getNombre()+"\n";
+            output += "categoria: "+tm.getCategoria();
+            output += "marca: "+tm.getMarca()+"\n";
+            output += "precio: "+tm.getPrecio()+"\n";
+            output += "stock: "+tm.getStock()+"\n";
+            output += "color: "+tm.getColor()+"\n";
+            output += "inalambrico: "+tm.getInalambrico()+"\n";
+            output += "dimension: "+tm.getDimension()+"\n";
+            output += "tipo: "+tm.getTipo()+"\n";
+            output += "switches: "+tm.getSwitches()+"\n";
+            output += "descripcion: "+tm.getDescripcion()+"\n";
+        }
+        if (output.isEmpty()){
+            return "Agregar Teclado!";
+        }
+        else {
+            return output;
+        }
+    }
+
+    //Listar por ID
+    public String getTecladoById(int id) {
+        String output = "";
+        if(TecladoRepository.existsById(id)){
+            TecladoModel tm = TecladoRepository.findById(id).get();
+            output += "id: "+tm.getId()+"\n";
+            output += "nombre: "+tm.getNombre()+"\n";
+            output += "categoria: "+tm.getCategoria();
+            output += "marca: "+tm.getMarca()+"\n";
+            output += "precio: "+tm.getPrecio()+"\n";
+            output += "stock: "+tm.getStock()+"\n";
+            output += "color: "+tm.getColor()+"\n";
+            output += "inalambrico: "+tm.getInalambrico()+"\n";
+            output += "dimension: "+tm.getDimension()+"\n";
+            output += "tipo: "+tm.getTipo()+"\n";
+            output += "switches: "+tm.getSwitches()+"\n";
+            output += "descripcion: "+tm.getDescripcion()+"\n";
+            return output;
+        }
+        else {
+            return "Teclado no encontrado";
+        }
+    }
+
+    //Agregar
+    public String addTeclado(TecladoModel tecladoModel){
+        TecladoRepository.save(tecladoModel);
+        return "Teclado agregegado";
+    }
+
+    //Delete
+    public String deleteTeclado(int id){
+        if (TecladoRepository.existsById(id)){
+            TecladoRepository.deleteById(id);
+            return "Teclado eliminado";
+        }else{
+            return "Teclado no encontrado";
+        }
+    }
+
+
+    //Update
+    public String updateTeclado(int id, TecladoModel nuevosDatosTeclado){
+        if (TecladoRepository.existsById(id)){
+
+            TecladoModel tecladoExistente = TecladoRepository.findById(id).get();
+
+            tecladoExistente.setNombre(nuevosDatosTeclado.getNombre());
+            tecladoExistente.setCategoria(nuevosDatosTeclado.getCategoria());
+            tecladoExistente.setMarca(nuevosDatosTeclado.getMarca());
+            tecladoExistente.setPrecio(nuevosDatosTeclado.getPrecio());
+
+            TecladoRepository.save(tecladoExistente);
+
+            return "Notebook actualizado exitosamente";
+        }
+        else {
+            return "Notebook no encontrado con ID: " + id;
+        }
+    }
+
+}
