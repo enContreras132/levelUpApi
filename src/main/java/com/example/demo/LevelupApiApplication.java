b8e130954f2742208fabf9d566a0466b212bf892
MaxiKresse <Max.kresse@duocuc.cl>
2025-11-28 20:28:22 -0300

    Se acrualizo seguridad propia de la api

--- src/main/java/com/example/demo/LevelupApiApplication.java
+++ src/main/java/com/example/demo/LevelupApiApplication.java
@@ -1,40 +1,14 @@
 package com.example.demo;
 
-import com.example.demo.security.JwtFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class LevelupApiApplication {
 
     public static void main(String[] args) {
-        
-        System.out.println(" --- INICIANDO LA APLICACIÓN LEVELUP (VERSIÓN SEGURA) ---");
         SpringApplication.run(LevelupApiApplication.class, args);
-    }
-
-    @Autowired
-    private JwtFilter jwtFilter;
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Si no ves este mensaje, la seguridad no se cargó
-        System.out.println("--- CARGANDO REGLAS DE SEGURIDAD Y JWT ---");
-
-        http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/login").permitAll() 
-                .anyRequest().authenticated()
-            )
-            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
     }
 }
