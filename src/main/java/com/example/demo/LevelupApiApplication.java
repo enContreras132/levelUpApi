d20ee312d62e5ff5087035168170882774490f7f
MaxiKresse <Max.kresse@duocuc.cl>
2025-11-28 19:30:37 -0300

    seguridad token
    tuve que borrar algo de seguridad por queme da error y editarlo de otra manera

--- src/main/java/com/example/demo/LevelupApiApplication.java
+++ src/main/java/com/example/demo/LevelupApiApplication.java
@@ -1,13 +1,39 @@
 package com.example.demo;
 
+import com.example.demo.security.JwtFilter; 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class LevelupApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LevelupApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LevelupApiApplication.class, args);
+    }
 
+    
+    
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        System.out.println("🔒 CARGANDO SEGURIDAD DESDE APP PRINCIPAL..."); 
+
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/login").permitAll() // DEJA PASAR A LOGIN SIN TOKEN
+                .anyRequest().authenticated()          // EL RESTO PIDE TOKEN
+            )
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
--- src/main/java/com/example/demo/security/JwtFilter.java
+++ src/main/java/com/example/demo/security/JwtFilter.java
@@ -12,7 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
+import com.example.demo.security.JwtFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 
