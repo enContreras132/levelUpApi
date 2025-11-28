ed110c554f75a7af3be763073ae4aeea0b70b2d7
Fabianxarii <fa.cuevast@duocuc.cl>
2025-11-27 22:52:28 -0300

    Notebook Service, Controller
    Notebook Service, Controller

--- src/main/java/com/example/demo/Controller/NotebookController.java
+++ src/main/java/com/example/demo/Controller/NotebookController.java
@@ -1,4 +1,38 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.NotebookModel;
+import com.example.demo.Service.NotebookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/notebook")
 public class NotebookController {
+    @Autowired
+    NotebookService NotebookService;
+
+    @GetMapping
+    public String getAllNotebooks(){
+        return NotebookService.getAllNotebooks();
+    }
+
+    @GetMapping("/{id}")
+    public String getNotebook(@PathVariable int id){
+        return NotebookService.getAudifonoById(id);
+    }
+
+    @PostMapping
+    public  String addNotebook(@RequestBody NotebookModel notebookModel){
+        return NotebookService.addNotebook(notebookModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteNotebook(@PathVariable int id){
+        return NotebookService.deleteNotebook(id); // Llama al servicio con el ID
+    }
+
+    @PutMapping("/{id}")
+    public String updateNotebook(@PathVariable int id, @RequestBody NotebookModel notebook){
+        return NotebookService.updateNotebook(id, notebook);
+    }
 }
---
+++ src/main/java/com/example/demo/Service/NotebookService.java
@@ -0,0 +1,98 @@
+package com.example.demo.Service;
+
+
+import com.example.demo.Model.NotebookModel;
+import com.example.demo.Repository.NotebookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class NotebookService {
+
+    @Autowired
+    NotebookRepository NotebookRepository;
+
+    //Listar
+    public String getAllNotebooks() {
+        String output = "";
+        for (NotebookModel nm : NotebookRepository.findAll()){
+            output += "id: "+nm.getId()+"\n";
+            output += "nombre: "+nm.getNombre()+"\n";
+            output += "categoria: "+nm.getCategoria();
+            output += "marca: "+nm.getMarca()+"\n";
+            output += "precio: "+nm.getPrecio()+"\n";
+            output += "stock: "+nm.getStock()+"\n";
+            output += "color: "+nm.getColor()+"\n";
+            output += "procesador: "+nm.getProcesador()+"\n";
+            output += "ram: "+nm.getRam()+"\n";
+            output += "memoria: "+nm.getMemoria()+"\n";
+            output += "tarjeta video: "+nm.getTarjetaVideo()+"\n";
+            output += "descripcion: "+nm.getDescripcion()+"\n";
+        }
+        if (output.isEmpty()){
+            return "Agregar Notebook!";
+        }
+        else {
+            return output;
+        }
+    }
+
+    //Listar por ID
+    public String getAudifonoById(int id) {
+        String output = "";
+        if (NotebookRepository.existsById(id)){
+            NotebookModel nm = NotebookRepository.findById(id).get();
+            output += "id: "+nm.getId()+"\n";
+            output += "nombre: "+nm.getNombre()+"\n";
+            output += "categoria: "+nm.getCategoria();
+            output += "marca: "+nm.getMarca()+"\n";
+            output += "precio: "+nm.getPrecio()+"\n";
+            output += "stock: "+nm.getStock()+"\n";
+            output += "color: "+nm.getColor()+"\n";
+            output += "procesador: "+nm.getProcesador()+"\n";
+            output += "ram: "+nm.getRam()+"\n";
+            output += "memoria: "+nm.getMemoria()+"\n";
+            output += "tarjeta video: "+nm.getTarjetaVideo()+"\n";
+            output += "descripcion: "+nm.getDescripcion()+"\n";
+            return output;
+        }
+        else {
+            return "Notebook no encontrado";
+        }
+    }
+    //Agregar
+    public String addNotebook(NotebookModel notebookModel){
+        NotebookRepository.save(notebookModel);
+        return "Notebook agregegado";
+    }
+
+    //Delete
+    public String deleteNotebook(int id){
+        if (NotebookRepository.existsById(id)){
+            NotebookRepository.deleteById(id);
+            return "Notebook eliminado";
+        }else{
+            return "Notebook no encontrado";
+        }
+    }
+
+    //Update
+    public String updateNotebook(int id, NotebookModel nuevosDatosNotebook){
+        if (NotebookRepository.existsById(id)){
+
+            NotebookModel notebookExistente = NotebookRepository.findById(id).get();
+
+            notebookExistente.setNombre(nuevosDatosNotebook.getNombre());
+            notebookExistente.setCategoria(nuevosDatosNotebook.getCategoria());
+            notebookExistente.setMarca(nuevosDatosNotebook.getMarca());
+            notebookExistente.setPrecio(nuevosDatosNotebook.getPrecio());
+
+            NotebookRepository.save(notebookExistente);
+
+            return "Notebook actualizado exitosamente";
+        }
+        else {
+            return "Notebook no encontrado con ID: " + id;
+        }
+    }
+
+
+}
