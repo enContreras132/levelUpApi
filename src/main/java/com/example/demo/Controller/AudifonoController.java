a68cd168249c14cba95948fa1db8b632e8f8753b
Fabianxarii <fa.cuevast@duocuc.cl>
2025-11-30 00:56:00 -0300

    prueba swagger
    prueba swagger

--- src/main/java/com/example/demo/Controller/AudifonoController.java
+++ src/main/java/com/example/demo/Controller/AudifonoController.java
@@ -2,6 +2,10 @@ package com.example.demo.Controller;
 
 import com.example.demo.Model.AudifonoModel;
 import com.example.demo.Service.AudifonoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +15,20 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/audifono")
 @CrossOrigin(origins = "http://localhost:5173")
+@Tag(name ="Audifonos", description = "Producto del sistema")
+
 public class AudifonoController {
 
     @Autowired
     AudifonoService AudifonoService;
 
     @GetMapping
+    @Operation(summary = "obtener todos los audifonos del sistema", description = "una lista con los datos de los audifonos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "se muestra la lista de audifonos"),
+            @ApiResponse(responseCode = "400", description = "No se ha podido obtener todos los audifonos del sistema")
+
+    })
     public List<AudifonoModel> getAllAudifono(){
         return AudifonoService.getAllAudifono();
     }
